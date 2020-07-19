2b588094878252d9ebee6efb23d9a60ca0a7e297
devprathamesh <bandivadekar.prathamesh@gmail.com>
2020-07-19 10:37:56 +0530

    First Commit

---
+++ src/main/java/dev/prathamesh/SpringDIBasics/SpringDiBasicsApplication.java
@@ -0,0 +1,13 @@
+package dev.prathamesh.SpringDIBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiBasicsApplication.class, args);
+	}
+
+}
